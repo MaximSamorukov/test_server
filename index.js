453f4e6b146681c368482a71e12a3d1f7b1c776c
maksim.samorukov <maksim.samorukov@sphaera.ru>
2024-03-08 18:58:21 +0300

    third commit

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-   console.log(req.path);
-   res.end('Hello');
+   console.log(req);
+   res.end('Hello', JSON.stringify(req));
 });
 
 server.listen(3000, () => console.log('Server starts at port 3000'))
