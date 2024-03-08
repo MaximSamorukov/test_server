4e7737f772a50037377434d5b955dd13a4068165
maksim.samorukov <maksim.samorukov@sphaera.ru>
2024-03-08 19:00:07 +0300

    fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
    console.log(req);
-   res.end('Hello', JSON.stringify(req));
+   res.end('Hello');
 });
 
 server.listen(3000, () => console.log('Server starts at port 3000'))
